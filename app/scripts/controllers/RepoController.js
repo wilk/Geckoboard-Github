ac3f7262cd8f355ef4c4dfb6407991c203ac706a
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-28 23:56:56 +0100

    added promise to retrieve every repo details

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -4,6 +4,20 @@ app.controller ('Repocontroller', ['$scope', 'repo', function ($scope, repo) {
     console.log (repo);
 
     repo.$promise.then (function (data) {
+        $q
+            .all ({
+                branches: $http.get (data.branches) ,
+                releases: $http.get (data.releases) ,
+                contributors: $http.get (data.contributors) ,
+                issues: $http.get (data.issues)
+            })
+            .then (function (res) {
+                $scope.repo.branches_count = res.branches.length;
+                $scope.repo.releases_count = res.releases.length;
+                $scope.repo.contributors_count = res.contributors.length;
+                $scope.repo.issues_count = res.issues.length;
+            });
+
         $scope.repo = data;
     }, function (error) {
         // @todo: handle the error when load single repo

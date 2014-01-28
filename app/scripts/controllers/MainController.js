239c1671a3debc3a4d78cf033cb4c8bcda86ea8f
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-28 23:29:34 +0100

    handled user and repos data incoming from github

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,10 +1,19 @@
 'use strict';
 
-angular.module('geckoboardGithubApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+app.controller ('MainController', ['$scope', 'user', 'repos', function ($scope, user, repos) {
+    console.log (user, repos);
+
+    user.$promise.then (function (data) {
+        data.avatar_url += '&s=440';
+
+        $scope.user = data;
+    }, function (error) {
+        // @todo: handle the error on user load
+    });
+
+    repos.$promise.then (function (data) {
+        $scope.repos = data;
+    }, function (error) {
+        // @todo: handle the error on repos load
+    });
+}]);

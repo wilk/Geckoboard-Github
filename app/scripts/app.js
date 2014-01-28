e67d181469da57cf997ad111516c365680dd393e
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-28 23:29:09 +0100

    added controllers, services, directives and routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,41 @@
 'use strict';
 
-angular.module('geckoboardGithubApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var app = angular
+    .module ('geckoboardGithubApp', [
+        'ngCookies' ,
+        'ngResource' ,
+        'ngSanitize' ,
+        'ngRoute'
+    ])
+    .constant ('GITHUB_API_URL', 'https://api.github.com')
+    .constant ('GITHUB_USER', 'mikermcneil')
+    .config (['$routeProvider', 'GITHUB_USER', function ($routeProvider, GITHUB_USER) {
+        $routeProvider
+            .when ('/', {
+                redirectTo: '/users/' + GITHUB_USER
+            })
+            .when ('/users/:user', {
+                templateUrl: 'views/main.html' ,
+                controller: 'MainController' ,
+                resolve: {
+                    user: ['$route', 'UserService', function ($route, UserService) {
+                        return UserService.get ({user: $route.current.params.user});
+                    }] ,
+                    repos: ['$route', 'ReposService', function ($route, ReposService) {
+                        return ReposService.query ({user: $route.current.params.user});
+                    }]
+                }
+            })
+            .when ('/repos/:user/:repo', {
+                templateUrl: 'views/repo.html' ,
+                controller: 'RepoController' ,
+                resolve: {
+                    repo: ['$route', 'RepoService', function ($route, RepoService) {
+                        return RepoService.get ({user: $route.current.params.user, repo: $route.current.params.repo});
+                    }]
+                }
+            })
+            .otherwise ({
+                redirectTo: '/'
+            });
+    }]);

49dd95ee13f86e9291014fa711c9c31da92fb785
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-30 01:05:23 +0100

    moved data requirements to routes

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -1,23 +1,9 @@
 'use strict';
 
-app.controller ('Repocontroller', ['$scope', '$q', '$http', 'repo', function ($scope, $q, $http, repo) {
+app.controller ('Repocontroller', ['$scope', 'repo', function ($scope, repo) {
     console.log (repo);
 
     repo.$promise.then (function (data) {
-        $q
-            .all ({
-                branches: $http.get (data.branches) ,
-                releases: $http.get (data.releases) ,
-                contributors: $http.get (data.contributors) ,
-                issues: $http.get (data.issues)
-            })
-            .then (function (res) {
-                $scope.repo.branches_count = res.branches.length;
-                $scope.repo.releases_count = res.releases.length;
-                $scope.repo.contributors_count = res.contributors.length;
-                $scope.repo.issues_count = res.issues.length;
-            });
-
         $scope.repo = data;
     }, function (error) {
         // @todo: handle the error when load single repo

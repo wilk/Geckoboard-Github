10d1a94cd3819ff2818512bb6ecb54e5f516abe1
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-30 01:07:05 +0100

    added requirements method (starred)

--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,5 +1,11 @@
 'use strict';
 
 app.service ('UserService', ['$resource', 'GITHUB_API_URL', function ($resource, GITHUB_API_URL) {
-    return $resource (GITHUB_API_URL + '/users/:user', {user: '@user'});
+    return $resource (GITHUB_API_URL + '/users/:user', {user: '@user'}, {
+        starred: {
+            method: 'GET' ,
+            url: GITHUB_API_URL + '/users/:user/starred' ,
+            isArray: true
+        }
+    });
 }]);

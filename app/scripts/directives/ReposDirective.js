4e07bc7ae50c486e71142d70466c1ee11808ec0d
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-30 01:05:54 +0100

    removed isolated scope

--- app/scripts/directives/ReposDirective.js
+++ app/scripts/directives/ReposDirective.js
@@ -4,9 +4,6 @@ app.directive ('ghRepos', function () {
     return {
         restrict: 'E' ,
         templateUrl: '../../views/partials/Repos.html' ,
-        scope: {
-            reposInfo: '=info'
-        } ,
         link: function (scope, element, attr) {
             scope.$watch ('repos', function (repos) {
                 if (repos) {

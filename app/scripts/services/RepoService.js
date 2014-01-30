1fff2e141336810c2c45c49affc815785bfd4d3a
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-30 01:06:36 +0100

    added requirements method (branches, releases, contributors and issues)

--- app/scripts/services/RepoService.js
+++ app/scripts/services/RepoService.js
@@ -1,5 +1,26 @@
 'use strict';
 
 app.service ('RepoService', ['$resource', 'GITHUB_API_URL', function ($resource, GITHUB_API_URL) {
-    return $resource (GITHUB_API_URL + '/repos/:user/:repo', {user: '@user', repo: '@repo'});
+    return $resource (GITHUB_API_URL + '/repos/:user/:repo', {user: '@user', repo: '@repo'}, {
+        branches: {
+            method: 'GET' ,
+            url: GITHUB_API_URL + '/repos/:user/:repo/branches' ,
+            isArray: true
+        } ,
+        releases: {
+            method: 'GET' ,
+            url: GITHUB_API_URL + '/repos/:user/:repo/releases' ,
+            isArray: true
+        } ,
+        contributors: {
+            method: 'GET' ,
+            url: GITHUB_API_URL + '/repos/:user/:repo/contributors' ,
+            isArray: true
+        } ,
+        issues: {
+            method: 'GET' ,
+            url: GITHUB_API_URL + '/repos/:user/:repo/issues' ,
+            isArray: true
+        }
+    });
 }]);

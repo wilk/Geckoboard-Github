eedd78df7fedc72c9ac94c96fb56cee461eaa69f
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-31 02:20:57 +0100

    removed unuseful code

--- app/scripts/directives/UserDirective.js
+++ app/scripts/directives/UserDirective.js
@@ -3,16 +3,6 @@
 app.directive ('ghUser', function () {
     return {
         restrict: 'E' ,
-        templateUrl: '../../views/partials/User.html' ,
-        scope: {
-            userInfo: '=info'
-        } ,
-        link: function (scope, element, attr) {
-            scope.$watch ('user', function (user) {
-                if (user) {
-                    //
-                }
-            });
-        }
+        templateUrl: '../../views/partials/User.html'
     };
 });

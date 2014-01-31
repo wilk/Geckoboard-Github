358166b5c899e1bc3e83d1aeae92771700f4b463
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-31 02:19:57 +0100

    handled user data

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -1,11 +1,17 @@
 'use strict';
 
-app.controller ('Repocontroller', ['$scope', 'repo', function ($scope, repo) {
-    console.log (repo);
-
+app.controller ('RepoController', ['$scope', 'repo', 'user', function ($scope, repo, user) {
     repo.$promise.then (function (data) {
         $scope.repo = data;
     }, function (error) {
         // @todo: handle the error when load single repo
+    });
+
+    user.$promise.then (function (data) {
+        data.avatar_url += '&s=440';
+
+        $scope.user = data;
+    }, function (error) {
+        // @todo: handle the error on user load
     });
 }]);

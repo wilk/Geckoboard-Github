38713d661a0a34dfb793b2cafd58018bb0aef35d
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-01-31 02:20:18 +0100

    added show/hide logic

--- app/scripts/directives/RepoDirective.js
+++ app/scripts/directives/RepoDirective.js
@@ -4,15 +4,36 @@ app.directive ('ghRepo', function () {
     return {
         restrict: 'E' ,
         templateUrl: '../../views/partials/Repo.html' ,
-        scope: {
-            repoInfo: '=info'
-        } ,
         link: function (scope, element, attr) {
-            scope.$watch ('repo', function (repo) {
-                if (repo) {
-                    //
+            scope.$on ('repo', function (event) {
+                if (event.currentScope.repo) {
+                    scope.repo = event.currentScope.repo;
+
+                    $('.repos').fadeOut (1000, function () {
+                        $('.repo').fadeIn ();
+                    });
+                }
+                else {
+                    $('.repo').fadeOut (1000, function () {
+                        $('.repos').fadeIn ();
+                    });
                 }
             });
+            /*scope.$watch (function (scope) {
+                return scope.repo;
+            }, function (repo) {
+                console.log ('repo', repo);
+                if (repo) {
+                    $('.repos').fadeOut (1000, function () {
+                        element.fadeIn (1000);
+                    });
+                }
+                else {
+                    element.fadeOut (1000, function () {
+                        $('.repos').fadeIn (1000);
+                    });
+                }
+            }, true);*/
         }
     };
 });

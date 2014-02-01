04cd3e3607875b450b43ee7eb9f5dcccc75e9325
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-01 21:50:32 +0100

    moved here logic to retrieve and parse incoming data for repos

--- app/scripts/services/ReposService.js
+++ app/scripts/services/ReposService.js
@@ -1,5 +1,31 @@
 'use strict';
 
-app.service ('ReposService', ['$resource', 'GITHUB_API_URL', function ($resource, GITHUB_API_URL) {
-    return $resource (GITHUB_API_URL + '/users/:user/repos', {user: '@user'});
+app.service ('ReposService', ['$resource', '$q', 'GITHUB_API_URL', function ($resource, $q, GITHUB_API_URL) {
+    var reposResource = $resource (GITHUB_API_URL + '/users/:user/repos', {user: '@user'}) ,
+        reposModel = {};
+
+    return {
+        data: function () {
+            return reposModel;
+        } ,
+        populate: function (user) {
+            var deferred = $q.defer ();
+
+            $q
+                .when (reposResource.query({user: user}).$promise)
+                .then (function (data) {
+                    angular.forEach (data, function (value, key) {
+                        data[key].iconCls = value.fork ? 'octicon-repo-forked' : 'octicon-repo';
+                    });
+
+                    reposModel = data;
+
+                    deferred.resolve (data);
+                }, function (error) {
+                    deferred.reject (error);
+                });
+
+            return deferred.promise;
+        }
+    };
 }]);

4d327a91c8937c7c65ace052edefffe2e939f79e
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-01 21:48:45 +0100

    simplified logic to show Repo page

--- app/scripts/directives/RepoDirective.js
+++ app/scripts/directives/RepoDirective.js
@@ -5,35 +5,8 @@ app.directive ('ghRepo', function () {
         restrict: 'E' ,
         templateUrl: '../../views/partials/Repo.html' ,
         link: function (scope, element, attr) {
-            scope.$on ('repo', function (event) {
-                if (event.currentScope.repo) {
-                    scope.repo = event.currentScope.repo;
-
-                    $('.repos').fadeOut (1000, function () {
-                        $('.repo').fadeIn ();
-                    });
-                }
-                else {
-                    $('.repo').fadeOut (1000, function () {
-                        $('.repos').fadeIn ();
-                    });
-                }
-            });
-            /*scope.$watch (function (scope) {
-                return scope.repo;
-            }, function (repo) {
-                console.log ('repo', repo);
-                if (repo) {
-                    $('.repos').fadeOut (1000, function () {
-                        element.fadeIn (1000);
-                    });
-                }
-                else {
-                    element.fadeOut (1000, function () {
-                        $('.repos').fadeIn (1000);
-                    });
-                }
-            }, true);*/
+            if (angular.isUndefined (scope.repo)) element.hide ();
+            else element.show ();
         }
     };
 });

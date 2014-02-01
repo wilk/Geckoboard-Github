e27f36c2ee2911009416d28fe6c6766bbdd2b663
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-01 21:48:06 +0100

    simplified logic

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -1,17 +1,6 @@
 'use strict';
 
 app.controller ('RepoController', ['$scope', 'repo', 'user', function ($scope, repo, user) {
-    repo.$promise.then (function (data) {
-        $scope.repo = data;
-    }, function (error) {
-        // @todo: handle the error when load single repo
-    });
-
-    user.$promise.then (function (data) {
-        data.avatar_url += '&s=440';
-
-        $scope.user = data;
-    }, function (error) {
-        // @todo: handle the error on user load
-    });
+    $scope.repo = repo;
+    $scope.user = user;
 }]);

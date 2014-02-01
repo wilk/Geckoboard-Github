3de71972a12c9df4e72cfbc0f279716d3608068f
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-01 21:50:44 +0100

    moved here logic to retrieve and parse incoming data for user

--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,11 +1,40 @@
 'use strict';
 
-app.service ('UserService', ['$resource', 'GITHUB_API_URL', function ($resource, GITHUB_API_URL) {
-    return $resource (GITHUB_API_URL + '/users/:user', {user: '@user'}, {
-        starred: {
-            method: 'GET' ,
-            url: GITHUB_API_URL + '/users/:user/starred' ,
-            isArray: true
+app.service ('UserService', ['$resource', '$q', 'GITHUB_API_URL', function ($resource, $q, GITHUB_API_URL) {
+    var userResource = $resource (GITHUB_API_URL + '/users/:user', {user: '@user'}, {
+            starred: {
+                method: 'GET' ,
+                url: GITHUB_API_URL + '/users/:user/starred' ,
+                isArray: true
+            }
+        }) ,
+        userModel = {};
+
+    return {
+        data: function () {
+            return userModel;
+        } ,
+        populate: function (user) {
+            var deferred = $q.defer () ,
+                userRequest = userResource.get ({user: user}) ,
+                starredRequest = userResource.starred ({user: user});
+
+            $q
+                .all ({
+                    user: userRequest.$promise ,
+                    starred: starredRequest.$promise
+                })
+                .then (function (data) {
+                    userModel = data.user;
+                    userModel.avatar_url += '&s=440';
+                    userModel.starred = data.starred.length;
+
+                    deferred.resolve (userModel);
+                }, function (error) {
+                    deferred.reject (error);
+                });
+
+            return deferred.promise;
         }
-    });
+    };
 }]);

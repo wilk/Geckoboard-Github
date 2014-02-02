fbf0c319f8f7c36385b0914a55bb3cab0be07a17
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-02 18:03:22 +0100

    added check for empty user data

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -5,6 +5,9 @@ app.controller ('MainController', ['$scope', 'user', 'repos', function ($scope, 
         repos[key].iconCls = value.fork ? 'octicon-repo-forked' : 'octicon-repo';
     });
 
+    if (angular.isUndefined (user.id)) user.hasData = false;
+    else user.hasData = true;
+
     $scope.user = user;
     $scope.repos = repos;
 }]);

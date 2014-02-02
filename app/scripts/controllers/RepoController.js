420b22f8b03a93d1425f6903af0e92c7babbabc6
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-02 18:03:38 +0100

    added check for empty user and repo data

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -1,6 +1,12 @@
 'use strict';
 
 app.controller ('RepoController', ['$scope', 'repo', 'user', function ($scope, repo, user) {
+    if (angular.isUndefined (user.id)) user.hasData = false;
+    else user.hasData = true;
+
+    if (angular.isUndefined (repo.id)) repo.hasData = false;
+    else repo.hasData = true;
+
     $scope.repo = repo;
     $scope.user = user;
 }]);

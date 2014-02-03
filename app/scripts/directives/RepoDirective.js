e4428479959bbb05038ae972daab03fc39c8dc14
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-04 00:20:13 +0100

    fixed templateUrl to work with build

--- app/scripts/directives/RepoDirective.js
+++ app/scripts/directives/RepoDirective.js
@@ -3,7 +3,7 @@
 app.directive ('ghRepo', function () {
     return {
         restrict: 'E' ,
-        templateUrl: '../../views/partials/Repo.html' ,
+        templateUrl: 'views/partials/Repo.html' ,
         link: function (scope, element, attr) {
             if (angular.isUndefined (scope.repo)) element.hide ();
             else element.show ();

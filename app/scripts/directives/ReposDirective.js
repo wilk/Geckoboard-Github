9219f025140ffefe0fb89b940e7de485b4d30c20
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-04 00:20:32 +0100

    fixed templateUrl to work with build

--- app/scripts/directives/ReposDirective.js
+++ app/scripts/directives/ReposDirective.js
@@ -3,7 +3,7 @@
 app.directive ('ghRepos', function () {
     return {
         restrict: 'E' ,
-        templateUrl: '../../views/partials/Repos.html' ,
+        templateUrl: 'views/partials/Repos.html' ,
         link: function (scope, element, attr) {
             scope.$on ('repos', function (event) {
                 if (event.currentScope.repos) {

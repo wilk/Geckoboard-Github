d4edfc8544f3a0d753c77476ac934060d7e0bede
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-05 00:40:28 +0100

    tested the whole main controller

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe ('Controller: MainController', function () {
-    var createController;
+    var fixtures , createController, $scope;
 
     // load the controller's module
     beforeEach (module ('geckoboardGithubApp'));
@@ -28,38 +28,106 @@ describe ('Controller: MainController', function () {
             user = user || {};
             repos = repos || [];
 
+            $scope = $rootScope.$new ();
+
+            fixtures = {
+                user: {
+                    "login": "mikermcneil",
+                    "id": 618009,
+                    "avatar_url": "https://gravatar.com/avatar/199046437b76e6ca73e00b4cc182a1c5?d=https%3A%2F%2Fidenticons.github.com%2F3559bfe5740ed5aac38d3e0e554dbaae.png&r=x",
+                    "starred_url": "https://api.github.com/users/mikermcneil/starred",
+                    "type": "User",
+                    "name": "Mike McNeil",
+                    "location": "Austin, TX",
+                    "email": "customers@balderdash.co",
+                    "followers": 222,
+                    "following": 161,
+                    "created_at": "2011-02-14T20:51:12Z"
+                },
+                repos: [
+                    {
+                        "name": "activityoverlord",
+                        "fork": true,
+                        "stargazers_count": 0,
+                        "language": "JavaScript",
+                        "forks_count": 0
+                    },
+                    {
+                        "name": "angularsailsdemo",
+                        "fork": true,
+                        "stargazers_count": 3,
+                        "language": "JavaScript",
+                        "forks_count": 2
+                    },
+                    {
+                        "name": "asset-rack",
+                        "fork": true,
+                        "stargazers_count": 0,
+                        "language": "CoffeeScript",
+                        "forks_count": 0
+                    },
+                    {
+                        "name": "backbone-ninjas-feb-27",
+                        "fork": false,
+                        "stargazers_count": 0,
+                        "language": "JavaScript",
+                        "forks_count": 0
+                    },
+                    {
+                        "name": "backbone-to-sails",
+                        "fork": false,
+                        "stargazers_count": 16,
+                        "language": "JavaScript",
+                        "forks_count": 2
+                    }
+                ],
+                repo: {
+                    "name": "sails",
+                    "description": "Realtime MVC Framework for Node.js",
+                    "fork": false,
+                    "stargazers_count": 4989,
+                    "forks_count": 534,
+                    "subscribers_count": 449
+                }
+            };
+
             return $controller ('MainController', {
-                $scope: $rootScope.$new () ,
+                $scope: $scope ,
                 user: user ,
                 repos: repos
             });
         };
     }));
 
-    describe ('when user and repos data are not yet retrieved', function () {
-        var controller = createController ();
-
+    describe ('when user and repos data are not loaded yet', function () {
         it ('should return empty user data', function () {
-            expect(controller.scope.user.hasData).toBe (false);
+            var controller = createController ();
+
+            expect($scope.user.hasData).toBe (false);
         });
 
         it ('should return empty repository list', function () {
-            expect(controller.scope.repos).toEqual (jasmine.any (Object));
-            expect(controller.scope.repos.length).toEqual (0);
+            var controller = createController ();
+
+            expect($scope.repos).toEqual (jasmine.any (Object));
+            expect($scope.repos.length).toEqual (0);
         });
     });
 
-    describe ('when user and repos are retrieved', function () {
-        var controller = createController ({id: '0101'}, [{fork: true},{fork: false}]);
+    describe ('when user and repos are loaded', function () {
+        it ('should set user hasData flag and format avatar url', function () {
+            var controller = createController (fixtures.user, fixtures.repos);
 
-        it ('should set user hasData flag', function () {
-            expect(controller.scope.user.hasData).toBe (true);
+            expect($scope.user.hasData).toBe (true);
+            expect($scope.user.avatar_url.substr (-6, 6)).toEqual ('&s=440');
         });
 
         it ('should append the right icon class to each repository', function () {
-            expect(controller.scope.repos).toEqual (jasmine.any (Object));
-            expect(controller.scope.repos.length).toBeGreaterThan (0);
-            expect(controller.scope.repos).toHaveEachRightIconClass ();
+            var controller = createController (fixtures.user, fixtures.repos);
+
+            expect($scope.repos).toEqual (jasmine.any (Object));
+            expect($scope.repos.length).toBeGreaterThan (0);
+            expect($scope.repos).toHaveEachRightIconClass ();
         });
     });
 });

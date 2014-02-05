95fde1c1a93304f54757e828617c614cdcab5b56
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-06 00:16:00 +0100

    added some comments

--- app/scripts/directives/RepoLinkDirective.js
+++ app/scripts/directives/RepoLinkDirective.js
@@ -1,9 +1,14 @@
 'use strict';
 
+/**
+ * Repo Link Directive
+ * It handles single repo link on the repos list
+ */
 app.directive ('ghRepoLink', function () {
     return {
         restrict: 'A' ,
         link: function (scope, element, attr) {
+            // Spinnerize the repo when it's clicked
             element.bind ('click', $.proxy (function () {
                 this.repository.iconCls = 'spinner';
             }, scope));

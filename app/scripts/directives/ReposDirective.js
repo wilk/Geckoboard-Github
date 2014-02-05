ef5615a8b92a16f065cb7e180c585c9a880125ef
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-06 00:16:46 +0100

    removed no more useful logic

--- app/scripts/directives/ReposDirective.js
+++ app/scripts/directives/ReposDirective.js
@@ -1,24 +1,12 @@
 'use strict';
 
+/**
+ * Repos Directive
+ * It handles repos list view
+ */
 app.directive ('ghRepos', function () {
     return {
         restrict: 'E' ,
-        templateUrl: 'views/partials/Repos.html' ,
-        link: function (scope, element, attr) {
-            scope.$on ('repos', function (event) {
-                if (event.currentScope.repos) {
-                    scope.repos = event.currentScope.repos;
-
-                    $('gh-repo').fadeOut (500, function () {
-                        element.fadeIn ();
-                    });
-                }
-                else {
-                    element.fadeOut (500, function () {
-                        $('.gh-repo').fadeIn ();
-                    });
-                }
-            });
-        }
+        templateUrl: 'views/partials/Repos.html'
     };
 });

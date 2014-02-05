3fd4a7963448eaeb7ccd7b54bd4702d58f8a6982
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-06 00:18:09 +0100

    moved default repos value from object to array

--- app/scripts/services/ReposService.js
+++ app/scripts/services/ReposService.js
@@ -1,13 +1,26 @@
 'use strict';
 
+/**
+ * Repos Service
+ * It loads every repository associated to the user
+ */
 app.service ('ReposService', ['$resource', '$q', 'GITHUB_API_URL', function ($resource, $q, GITHUB_API_URL) {
     var reposResource = $resource (GITHUB_API_URL + '/users/:user/repos?page=0&per_page=100', {user: '@user'}) ,
-        reposModel = {};
+        reposModel = [];
 
     return {
+        /**
+         * Get repos model
+         * @returns {Object} Repos model
+         */
         data: function () {
             return reposModel;
         } ,
+        /**
+         * Populate repos model
+         * @param {String} user The user that owns the repos
+         * @returns {promise} A promise that it will be resolved with repos model
+         */
         populate: function (user) {
             var deferred = $q.defer ();
 
@@ -18,7 +31,7 @@ app.service ('ReposService', ['$resource', '$q', 'GITHUB_API_URL', function ($re
 
                     deferred.resolve (reposModel);
                 }, function (error) {
-                    reposModel = {};
+                    reposModel = [];
                     deferred.resolve (reposModel);
                 });
 

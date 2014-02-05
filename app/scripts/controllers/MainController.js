8d620826dd7e0eb2b6f080f22242824809dbc768
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-06 00:14:43 +0100

    fixed avatar_url formatting

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,16 +1,22 @@
 'use strict';
 
+/**
+ * Main Controller
+ * It handles user and repos models
+ */
 app.controller ('MainController', ['$scope', 'user', 'repos', function ($scope, user, repos) {
+    // Select the right icon css class to attach to each repo
     angular.forEach (repos, function (value, key) {
         repos[key].iconCls = value.fork ? 'octicon-repo-forked' : 'octicon-repo';
     });
 
+    // Check if user data has been loaded
     if (angular.isUndefined (user.id)) user.hasData = false;
     else {
         user.hasData = true;
 
-        if (angular.isUndefined ($scope.user)) user.avatar_url += '&s=440';
-        else if ($scope.user.id !== user.id) user.avatar_url += '&s=440';
+        // Load a 220x220 avatar
+        if (user.avatar_url.substr (-6, 6) !== '&s=440') user.avatar_url += '&s=440';
     }
 
     $scope.user = user;

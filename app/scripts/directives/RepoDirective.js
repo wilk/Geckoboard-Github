9047ed40bf0d0fec231283f6cc90b88f99e3b540
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-02-06 00:15:45 +0100

    added some comments

--- app/scripts/directives/RepoDirective.js
+++ app/scripts/directives/RepoDirective.js
@@ -1,10 +1,15 @@
 'use strict';
 
+/**
+ * Repo Directive
+ * It handles single repo view
+ */
 app.directive ('ghRepo', function () {
     return {
         restrict: 'E' ,
         templateUrl: 'views/partials/Repo.html' ,
         link: function (scope, element, attr) {
+            // Hide or show repo page
             if (angular.isUndefined (scope.repo)) element.hide ();
             else element.show ();
         }
